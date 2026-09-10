6d572997cae71eea3596a1b0cd336084bc6cd34b
Minsoo Choo <mchoo@FreeBSD.org>
2026-09-10 13:16:05 -0400

    trap_cfi(9): add exception handler CFI for amd64
    MFC after:	2 weeks
    MFC to:	stable/14, stable/15
    Sponsored by:	FreeBSD Foundation

--- sys/amd64/include/cpu.h
+++ sys/amd64/include/cpu.h
@@ -78,6 +78,7 @@ void	cpu_halt(void);
 void	cpu_lock_delay(void);
 void	cpu_reset(void);
 void	fork_trampoline(void);
+void	fork_trampoline_kthread(void);
 
 /*
  * Return contents of in-cpu fast counter as a sort of "bogo-time"
